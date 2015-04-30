b99bf02179d402be192d6442a3e22c54905e9feb
Lasse V. Karlsen <lasse@vkarlsen.no>
2015-04-30 08:30:33 -0200

    Added tests for OpenNow.

---
+++ Sequel.Tests/OpenNowTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+using NSubstitute;
+using NUnit.Framework;
+
+// ReSharper disable AssignNullToNotNullAttribute
+
+namespace Sequel.Tests
+{
+    [TestFixture]
+    public class OpenNowTests
+    {
+        [Test]
+        public void OpenNow_NullConnection_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => DbConnectionExtensions.OpenNow<IDbConnection>(null));
+        }
+
+        [Test]
+        public void OpenNow_WithConnection_CallsOpenOnConnection()
+        {
+            var connection = Substitute.For<IDbConnection>();
+            
+            connection.OpenNow();
+
+            connection.Received().Open();
+        }
+    }
+}
