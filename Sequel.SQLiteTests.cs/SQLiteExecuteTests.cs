3e86c25e3af5b2547db643eeb65fe2c395ce6d63
Lasse V. Karlsen <lasse@vkarlsen.no>
2015-04-30 08:30:13 -0200

    Added some SQLite specific tests.

---
+++ Sequel.SQLiteTests.cs/SQLiteExecuteTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+
+namespace Sequel.SQLiteTests.cs
+{
+    [TestFixture]
+    public class SQLiteExecuteTests : SQLiteTestBase
+    {
+        [Test]
+        public void QueryScalar_OnCreatedTableWithOneRow_ReturnsOne()
+        {
+            Connection.Execute("CREATE TABLE some_table (Key TEXT, Value INTEGER)");
+            Connection.Execute("INSERT INTO some_table VALUES (@Key, @Value)", new { Key = "Meaning of Life", Value = 42 });
+            var rowCount = Connection.QueryScalar<int>("SELECT COUNT(*) FROM some_table");
+
+            Assert.That(rowCount, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void QuerySequenceForFirstColumn_OnCreatedTableWithTwoRows_ReturnsTheValuesOfTheFirstColumn()
+        {
+            Connection.Execute("CREATE TABLE some_table (Key TEXT, Value INTEGER)");
+            using (var insert = Connection.PrepareForExecute("INSERT INTO some_table VALUES (@Key, @Value)", new { Key = "", Value = 0 }))
+            {
+                insert.Execute(new { Key = "Meaning of Life 1", Value = 42 });
+                insert.Execute(new { Key = "Meaning of Life 2", Value = 84 });
+            }
+
+            var rows = Connection.QuerySequence<string>("SELECT Key FROM some_table ORDER BY Key");
+
+            CollectionAssert.AreEqual(new[]
+            {
+                "Meaning of Life 1",
+                "Meaning of Life 2"
+            }, rows);
+        }
+
+        [Test]
+        public void QuerySequenceForSecondColumn_OnCreatedTableWithTwoRows_ReturnsTheValuesOfTheSecondColumn()
+        {
+            Connection.Execute("CREATE TABLE some_table (Key TEXT, Value INTEGER)");
+            using (var insert = Connection.PrepareForExecute("INSERT INTO some_table VALUES (@Key, @Value)", new { Key = "", Value = 0 }))
+            {
+                insert.Execute(new { Key = "Meaning of Life 1", Value = 42 });
+                insert.Execute(new { Key = "Meaning of Life 2", Value = 84 });
+            }
+
+            var rows = Connection.QuerySequence<int>("SELECT Value FROM some_table ORDER BY Key");
+
+            CollectionAssert.AreEqual(new[]
+            {
+                42,
+                84
+            }, rows);
+        }
+    }
+}
---
+++ Sequel.SQLiteTests.cs/SQLiteTestBase.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.SQLite;
+using System.IO;
+using NUnit.Framework;
+
+namespace Sequel.SQLiteTests.cs
+{
+    public class SQLiteTestBase
+    {
+        private string _FileName;
+        private SQLiteConnection _Connection;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _FileName = Path.GetTempFileName();
+            _Connection = new SQLiteConnection("Data Source=" + _FileName).OpenNow();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _Connection.Dispose();
+            if (File.Exists(_FileName))
+                File.Delete(_FileName);
+        }
+
+        protected SQLiteConnection Connection
+        {
+            get
+            {
+                return _Connection;
+            }
+        }
+    }
+}
